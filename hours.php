47b8b873c89fb23dfb1520fc2f91aac0d96fb6fe
Kim <kimwalta@gmail.com>
2012-03-22 16:25:13 -0500

    Edited pages
    Moved jquery to cdn, lots of editing of index and style.css

--- hours.php
+++ hours.php
@@ -1,64 +1,22 @@
-<p><h3><?php print "Today's Hours: ". date('l, F jS, Y'); ?></h3></p>
-<hr>
-<!-- Hours for Pius -->
-<p>
+<style type="text/css">
+ul { color: #FFF; list-style:none; font-family: Arial, "MS Trebuchet", sans-serif; text-align: center; }
+</style>
 <?php
 $today = date("Y-m-d");
-    $feedURL = "http://www.google.com/calendar/feeds/slu.edu_eadq0giqc5h6bf3l3f50jk34jo%40group.calendar.google.com/public/basic?start-min=".$today."T00:00:00&start-max=".$today."T23:59:59&ctz=America/Chicago";
-			    //print $feedURL;
-				$sxml = simplexml_load_file($feedURL);
+    $feedURL = "http://www.google.com/calendar/feeds/slu.edu_c4reknqcm60rf0blukgoio5urs%40group.calendar.google.com/public/basic?start-min=".$today."T00:00:00&start-max=".$today."T23:59:59&ctz=America/Chicago";
+          //print $feedURL;
+        $sxml = simplexml_load_file($feedURL);
 
    $counts = $sxml->children('http://a9.com/-/spec/opensearchrss/1.0/');
    $total = $counts->totalResults; 
 ?>
-
+      
+<ul>
+ <li><p><?php print "Hours for ". date('M jS') .":</BR>"; ?></p></li>           
 <?php
 
 foreach ($sxml->entry as $entry) {
   $caltitle = stripslashes($entry->title);
-   echo "<strong>Pius XII Memorial Library</strong><br />$caltitle";
+   echo "<li>$caltitle</li>";
 }
 ?>
-</p>
-
-<!-- Hours for MCL -->
-<p>
-<?php
-$today = date("Y-m-d");
-    $feedURL = "http://www.google.com/calendar/feeds/slu.edu_ffd8isj5p44ee2vohkv673jclk%40group.calendar.google.com/public/basic?start-min=".$today."T00:00:00&start-max=".$today."T23:59:59&ctz=America/Chicago";
-			    //print $feedURL;
-				$sxml = simplexml_load_file($feedURL);
-
-   $counts = $sxml->children('http://a9.com/-/spec/opensearchrss/1.0/');
-   $total = $counts->totalResults; 
-?>
-
-<?php
-
-foreach ($sxml->entry as $entry) {
-  $caltitle = stripslashes($entry->title);
-   echo "<strong>Medical Center Library</strong><br />$caltitle";
-}
-?>
-</p>
-
-<!-- Hours for Law -->
-<p>
-<?php
-$today = date("Y-m-d");
-    $feedURL = "http://www.google.com/calendar/feeds/slu.edu_t2ahlbgihctv2037qf8cv520s0%40group.calendar.google.com/public/basic?start-min=".$today."T00:00:00&start-max=".$today."T23:59:59&ctz=America/Chicago";
-			    //print $feedURL;
-				$sxml = simplexml_load_file($feedURL);
-
-   $counts = $sxml->children('http://a9.com/-/spec/opensearchrss/1.0/');
-   $total = $counts->totalResults; 
-?>
-
-<?php
-
-foreach ($sxml->entry as $entry) {
-  $caltitle = stripslashes($entry->title);
-   echo "<strong>Omer Poos Law Library</strong><br />$caltitle";
-}
-?>
-</p>
